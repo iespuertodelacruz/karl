bdc72c930fb68b34914feb47a07f5d1859145275
Sergio Delgado Quintero <sdelquin@gmail.com>
2019-02-24 19:54:47 +0000

    Add auth token

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import json
+import re
 
 from flask import Flask, request
 
 import bot
 import config
-import re
 import utils
 
 app = Flask(__name__)
@@ -12,13 +12,15 @@ app = Flask(__name__)
 
 @app.route('/api/send_message', methods=['POST'])
 def send_message():
+    token = request.form.get('token', '')
+    if token != config.AUTH_TOKEN:
+        return json.dumps({'status': 'error', 'msg': 'bad auth token'}), 401
     msg = request.form.get('msg', '')
     groups = re.split(r'\s*,\s*', request.form.get('groups', '').strip())
     for group in groups:
         chat_id = delivery_groups.get(group)
-        print(chat_id)
         bot.send_message(chat_id, msg)
-    return json.dumps({'status': 'ok'})
+    return json.dumps({'status': 'success'}), 200
 
 
 delivery_groups = utils.load_groups(config.GROUPS_FILENAME)
